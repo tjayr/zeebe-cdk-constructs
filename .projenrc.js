3e01a6fd1195a11f7f537eb6241ab5c7c89ae349
Tony Ayres <tayres@gmail.com>
2022-06-19 20:09:46 +0100

    chore: update gitignore for .idea

--- .projenrc.js
+++ .projenrc.js
@@ -6,6 +6,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: 'zeebe-cdk-constructs',
   repositoryUrl: 'https://github.com/tjayr/zeebe-cdk-constructs.git',
+  gitignore: ['.idea'],
+
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
