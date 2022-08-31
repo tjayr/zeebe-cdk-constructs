c084b0cc7a65ac03455f5d48f8b371557858be95
Tony Ayres <tony.ayres@dcu.ie>
2022-08-31 16:40:17 +0100

    fix: configure gh action release workflow (#37)
    chore: configure gh action release workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,40 +1,47 @@
-const {awscdk, RenovatebotScheduleInterval} = require('projen');
-const {ReleaseTrigger} = require("projen/lib/release");
+const { awscdk, RenovatebotScheduleInterval } = require('projen');
 
 const project = new awscdk.AwsCdkConstructLibrary({
-    author: 'Tony Ayres',
-    authorAddress: 'tayres@gmail.com',
-    cdkVersion: '2.39.1',
-    defaultReleaseBranch: 'main',
-    name: 'zeebe-cdk-constructs',
-    repositoryUrl: 'https://github.com/tjayr/zeebe-cdk-constructs.git',
-    gitignore: ['.idea'],
-    keywords: ['cdk', 'aws-cdk', 'awscdk', 'camunda', 'zeebe'],
-    stability: 'experimental',
+  author: 'Tony Ayres',
+  authorAddress: 'tayres@gmail.com',
+  cdkVersion: '2.39.1',
+  defaultReleaseBranch: 'main',
+  name: 'zeebe-cdk-constructs',
+  repositoryUrl: 'https://github.com/tjayr/zeebe-cdk-constructs.git',
+  gitignore: ['.idea'],
+  keywords: ['cdk', 'aws-cdk', 'awscdk', 'camunda', 'zeebe'],
+  stability: 'experimental',
 
-    // deps: [],                /* Runtime dependencies of this module. */
-    // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-    // devDeps: [],             /* Build dependencies for this module. */
-    // packageName: undefined,  /* The "name" in package.json. */
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
 
-    releaseBranches: 'main',
-    releaseToNpm: true,
-    releaseWorkflow: true,
-    releaseTrigger: ReleaseTrigger.manual(),
+  releaseBranches: 'main',
+  releaseToNpm: true,
+  releaseWorkflow: true,
+  //releaseTrigger: ReleaseTrigger.manual(),
 
-    // publishToMaven: {
-    //   javaPackage: 'com.github.tjayr.zeebe',
-    //   mavenGroupId: 'com.github.tjayr',
-    //   mavenArtifactId: 'zeebe-cdk-constructs',
-    // },
-    renovatebot: true,
-    renovatebotOptions: {
-        scheduleInterval: [RenovatebotScheduleInterval.WEEKENDS],
-    }, //,
-    docgen: true,
-    dependabot: false,
-    release: true,
-    buildWorkflow: false,
+  // publishToMaven: {
+  //   javaPackage: 'com.github.tjayr.zeebe',
+  //   mavenGroupId: 'com.github.tjayr',
+  //   mavenArtifactId: 'zeebe-cdk-constructs',
+  // },
+  renovatebot: true,
+  renovatebotOptions: {
+    scheduleInterval: [RenovatebotScheduleInterval.WEEKENDS],
+  },
+  docgen: true,
+  dependabot: false,
+  release: true,
+  buildWorkflow: false,
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ['chore', 'feat', 'fix', 'ci', 'docs', 'build', 'style', 'refactor', 'perf', 'test'],
+      },
+      semanticTitle: true,
+    },
+  },
 });
 
 
