6fdfd693fd4aac95e642b62e9b7ae2db383f968b
Tony Ayres <tayres@gmail.com>
2022-09-01 21:56:38 +0100

    test: remove CDK_DEFAULT_ACCOUNT env var from tests

--- test/camunda-platform.test.ts
+++ test/camunda-platform.test.ts
@@ -1,70 +1,70 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { CamundaPlatformCoreFargate } from '../src/camunda-platform';
+import {Template} from 'aws-cdk-lib/assertions';
+import {CamundaPlatformCoreFargate} from '../src/camunda-platform';
 
 describe('CamundaPlatform', () => {
 
-  test('Synthesize a camunda core cluster', () => {
+    test('Synthesize a camunda core cluster', () => {
 
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'CamundaCoreStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'CamundaCoreStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
+
+        new CamundaPlatformCoreFargate(clusterStack, 'CamundaCoreCluster', {});
+
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+
+        template.resourceCountIs('AWS::EFS::FileSystem', 1);
+        template.resourceCountIs('AWS::EFS::MountTarget', 3);
+        template.resourceCountIs('AWS::ECS::Service', 4);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 4);
+
+        template.hasResource('AWS::ECS::Service', {Properties: {ServiceName: 'elasticsearch'}});
+        template.hasResource('AWS::ECS::Service', {Properties: {ServiceName: 'operate'}});
+        template.hasResource('AWS::ECS::Service', {Properties: {ServiceName: 'zeebe'}});
+        template.hasResource('AWS::ECS::Service', {Properties: {ServiceName: 'tasklist'}});
+
+        template.hasResource('AWS::EFS::FileSystem', {
+            Properties: {
+                FileSystemTags: [
+                    {
+                        Key: 'Name',
+                        Value: 'camunda-core-efs',
+                    },
+                ],
+            },
+        });
+
+        template.hasResource('AWS::EFS::AccessPoint', {
+            Properties: {
+                RootDirectory: {
+                    Path: '/broker-data',
+                },
+            },
+        });
+
+        template.hasResource('AWS::EFS::AccessPoint', {
+            Properties: {
+                RootDirectory: {
+                    Path: '/elasticsearch',
+                },
+            },
+        });
+
+        template.hasResource('AWS::ServiceDiscovery::PrivateDnsNamespace', {
+            Properties: {
+                Name: 'camunda-cluster.net',
+            },
+        });
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'camunda-cluster',
+        });
     });
-
-    new CamundaPlatformCoreFargate(clusterStack, 'CamundaCoreCluster', {});
-
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-
-    template.resourceCountIs('AWS::EFS::FileSystem', 1);
-    template.resourceCountIs('AWS::EFS::MountTarget', 3);
-    template.resourceCountIs('AWS::ECS::Service', 4);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 4);
-
-    template.hasResource('AWS::ECS::Service', { Properties: { ServiceName: 'elasticsearch' } });
-    template.hasResource('AWS::ECS::Service', { Properties: { ServiceName: 'operate' } });
-    template.hasResource('AWS::ECS::Service', { Properties: { ServiceName: 'zeebe' } });
-    template.hasResource('AWS::ECS::Service', { Properties: { ServiceName: 'tasklist' } });
-
-    template.hasResource('AWS::EFS::FileSystem', {
-      Properties: {
-        FileSystemTags: [
-          {
-            Key: 'Name',
-            Value: 'camunda-core-efs',
-          },
-        ],
-      },
-    });
-
-    template.hasResource('AWS::EFS::AccessPoint', {
-      Properties: {
-        RootDirectory: {
-          Path: '/broker-data',
-        },
-      },
-    });
-
-    template.hasResource('AWS::EFS::AccessPoint', {
-      Properties: {
-        RootDirectory: {
-          Path: '/elasticsearch',
-        },
-      },
-    });
-
-    template.hasResource('AWS::ServiceDiscovery::PrivateDnsNamespace', {
-      Properties: {
-        Name: 'camunda-cluster.net',
-      },
-    });
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'camunda-cluster',
-    });
-  });
 
 
 });
--- test/standalone.test.ts
+++ test/standalone.test.ts
@@ -1,244 +1,244 @@
 import * as cdk from 'aws-cdk-lib';
-import { Match, Template } from 'aws-cdk-lib/assertions';
-import { Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { Cluster } from 'aws-cdk-lib/aws-ecs';
-import { FileSystem } from 'aws-cdk-lib/aws-efs';
-import { ZeebeStandaloneFargateCluster } from '../src/standalone';
+import {Match, Template} from 'aws-cdk-lib/assertions';
+import {Peer, Port, SecurityGroup, Vpc} from 'aws-cdk-lib/aws-ec2';
+import {Cluster} from 'aws-cdk-lib/aws-ecs';
+import {FileSystem} from 'aws-cdk-lib/aws-efs';
+import {ZeebeStandaloneFargateCluster} from '../src/standalone';
 
 describe('ZeebeCluster', () => {
 
-  test('Synthesize a standalone zeebe cluster with a custom efs file system', () => {
+    test('Synthesize a standalone zeebe cluster with a custom efs file system', () => {
 
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
-    });
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
 
-    const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
-    const ecsCluster = new Cluster(clusterStack, 'Cluster', { clusterName: 'test-cluster' });
-    const sg = new SecurityGroup(clusterStack, 'test-sg', {
-      vpc: defaultVpc,
-      securityGroupName: 'test-sg',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
+        const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
+        const ecsCluster = new Cluster(clusterStack, 'Cluster', {clusterName: 'test-cluster'});
+        const sg = new SecurityGroup(clusterStack, 'test-sg', {
+            vpc: defaultVpc,
+            securityGroupName: 'test-sg',
+            allowAllOutbound: true,
+        });
+        sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
 
-    let fileSystem: FileSystem = new FileSystem(clusterStack, 'efs', {
-      vpc: defaultVpc,
-    });
+        let fileSystem: FileSystem = new FileSystem(clusterStack, 'efs', {
+            vpc: defaultVpc,
+        });
 
-    new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
-      vpc: defaultVpc,
-      ecsCluster: ecsCluster,
-      securityGroups: [sg],
-      fileSystem: fileSystem,
-    });
+        new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
+            vpc: defaultVpc,
+            ecsCluster: ecsCluster,
+            securityGroups: [sg],
+            fileSystem: fileSystem,
+        });
 
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-    template.resourceCountIs('AWS::EFS::FileSystem', 1);
-    template.resourceCountIs('AWS::EFS::MountTarget', 3);
-    template.resourceCountIs('AWS::ECS::Service', 1);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
-    template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'test-cluster',
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [{ Name: 'zeebe-standalone', Memory: 1024 }],
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [Match.not({ Name: 'simple-monitor' })],
-    });
-  });
-
-
-  test('Synthesize a standalone zeebe cluster with default settings', () => {
-
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+        template.resourceCountIs('AWS::EFS::FileSystem', 1);
+        template.resourceCountIs('AWS::EFS::MountTarget', 3);
+        template.resourceCountIs('AWS::ECS::Service', 1);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+        template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'test-cluster',
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [{Name: 'zeebe-standalone', Memory: 1024}],
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [Match.not({Name: 'simple-monitor'})],
+        });
     });
 
-    const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
-    const sg = new SecurityGroup(clusterStack, 'test-sg', {
-      vpc: defaultVpc,
-      securityGroupName: 'test-sg',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
 
-    new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
-      securityGroups: [sg],
-      vpc: defaultVpc,
-    });
+    test('Synthesize a standalone zeebe cluster with default settings', () => {
 
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-    template.resourceCountIs('AWS::EFS::FileSystem', 0);
-    template.resourceCountIs('AWS::EFS::MountTarget', 0);
-    template.resourceCountIs('AWS::ECS::Service', 1);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
-    template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'zeebe-standalone',
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [{ Name: 'zeebe-standalone', Memory: 1024 }],
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [Match.not({ Name: 'simple-monitor' })],
-    });
-  });
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
 
-  test('Synthesize a standalone zeebe cluster with default settings and private DNS enabled', () => {
+        const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
+        const sg = new SecurityGroup(clusterStack, 'test-sg', {
+            vpc: defaultVpc,
+            securityGroupName: 'test-sg',
+            allowAllOutbound: true,
+        });
+        sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
 
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
-    });
+        new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
+            securityGroups: [sg],
+            vpc: defaultVpc,
+        });
 
-    const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
-    const sg = new SecurityGroup(clusterStack, 'test-sg', {
-      vpc: defaultVpc,
-      securityGroupName: 'test-sg',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
-
-    new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
-      securityGroups: [sg],
-      vpc: defaultVpc,
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+        template.resourceCountIs('AWS::EFS::FileSystem', 0);
+        template.resourceCountIs('AWS::EFS::MountTarget', 0);
+        template.resourceCountIs('AWS::ECS::Service', 1);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+        template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'zeebe-standalone',
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [{Name: 'zeebe-standalone', Memory: 1024}],
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [Match.not({Name: 'simple-monitor'})],
+        });
     });
 
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-    template.resourceCountIs('AWS::EFS::FileSystem', 0);
-    template.resourceCountIs('AWS::EFS::MountTarget', 0);
-    template.resourceCountIs('AWS::ECS::Service', 1);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
-    template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'zeebe-standalone',
+    test('Synthesize a standalone zeebe cluster with default settings and private DNS enabled', () => {
+
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
+
+        const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
+        const sg = new SecurityGroup(clusterStack, 'test-sg', {
+            vpc: defaultVpc,
+            securityGroupName: 'test-sg',
+            allowAllOutbound: true,
+        });
+        sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
+
+        new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
+            securityGroups: [sg],
+            vpc: defaultVpc,
+        });
+
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+        template.resourceCountIs('AWS::EFS::FileSystem', 0);
+        template.resourceCountIs('AWS::EFS::MountTarget', 0);
+        template.resourceCountIs('AWS::ECS::Service', 1);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+        template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'zeebe-standalone',
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            Cpu: '512',
+            Memory: '1024',
+            ContainerDefinitions: [{Name: 'zeebe-standalone', Memory: 1024}],
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [Match.not({Name: 'simple-monitor'})],
+        });
     });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      Cpu: '512',
-      Memory: '1024',
-      ContainerDefinitions: [{ Name: 'zeebe-standalone', Memory: 1024 }],
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [Match.not({ Name: 'simple-monitor' })],
-    });
-  });
 
 
-  test('Standalone zeebe cluster using a bind mount volume for storage', () => {
+    test('Standalone zeebe cluster using a bind mount volume for storage', () => {
 
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
+
+        const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
+        const ecsCluster = new Cluster(clusterStack, 'Cluster', {clusterName: 'test-cluster'});
+        const sg = new SecurityGroup(clusterStack, 'test-sg', {
+            vpc: defaultVpc,
+            securityGroupName: 'test-sg',
+            allowAllOutbound: true,
+        });
+        sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
+
+        new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
+            vpc: defaultVpc,
+            ecsCluster: ecsCluster,
+            securityGroups: [sg],
+        });
+
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+        template.resourceCountIs('AWS::EFS::FileSystem', 0);
+        template.resourceCountIs('AWS::EFS::MountTarget', 0);
+        template.resourceCountIs('AWS::ECS::Service', 1);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+        template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'test-cluster',
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            Cpu: '512',
+            Memory: '1024',
+            ContainerDefinitions: [{Name: 'zeebe-standalone', Memory: 1024}],
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [Match.not({Name: 'simple-monitor'})],
+        });
+
     });
 
-    const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
-    const ecsCluster = new Cluster(clusterStack, 'Cluster', { clusterName: 'test-cluster' });
-    const sg = new SecurityGroup(clusterStack, 'test-sg', {
-      vpc: defaultVpc,
-      securityGroupName: 'test-sg',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
+    test('Standalone with simple monitor task', () => {
 
-    new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
-      vpc: defaultVpc,
-      ecsCluster: ecsCluster,
-      securityGroups: [sg],
-    });
+        const app = new cdk.App();
+        const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
+            env: {
+                account: '12345',
+                region: 'eu-west-1'
+            },
+        });
 
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-    template.resourceCountIs('AWS::EFS::FileSystem', 0);
-    template.resourceCountIs('AWS::EFS::MountTarget', 0);
-    template.resourceCountIs('AWS::ECS::Service', 1);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
-    template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'test-cluster',
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      Cpu: '512',
-      Memory: '1024',
-      ContainerDefinitions: [{ Name: 'zeebe-standalone', Memory: 1024 }],
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [Match.not({ Name: 'simple-monitor' })],
-    });
+        const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
+        const ecsCluster = new Cluster(clusterStack, 'Cluster', {clusterName: 'test-cluster'});
+        const sg = new SecurityGroup(clusterStack, 'test-sg', {
+            vpc: defaultVpc,
+            securityGroupName: 'test-sg',
+            allowAllOutbound: true,
+        });
+        sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
 
-  });
+        new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
+            vpc: defaultVpc,
+            ecsCluster: ecsCluster,
+            securityGroups: [sg],
+            simpleMonitor: true,
+            hazelcastExporter: true,
+        });
 
-  test('Standalone with simple monitor task', () => {
+        // Prepare the stack for assertions.
+        let template = Template.fromStack(clusterStack);
+        template.resourceCountIs('AWS::EFS::FileSystem', 0);
+        template.resourceCountIs('AWS::EFS::MountTarget', 0);
+        template.resourceCountIs('AWS::ECS::Service', 1);
+        template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+        template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
+        template.hasResourceProperties('AWS::ECS::Cluster', {
+            ClusterName: 'test-cluster',
+        });
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            ContainerDefinitions: [
+                {
+                    Name: 'zeebe-standalone',
+                    Memory: 1024,
+                    Image: 'ghcr.io/camunda-community-hub/zeebe-with-hazelcast-exporter:8.0.5',
+                },
+                {
+                    Name: 'simple-monitor',
+                    Memory: 1024,
+                },
+            ],
+        });
 
-    const app = new cdk.App();
-    const clusterStack = new cdk.Stack(app, 'ZeebeStandaloneStack', {
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
     });
-
-    const defaultVpc = new Vpc(clusterStack, 'test-vpc', {});
-    const ecsCluster = new Cluster(clusterStack, 'Cluster', { clusterName: 'test-cluster' });
-    const sg = new SecurityGroup(clusterStack, 'test-sg', {
-      vpc: defaultVpc,
-      securityGroupName: 'test-sg',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(Peer.anyIpv4(), Port.tcpRange(26500, 26502));
-
-    new ZeebeStandaloneFargateCluster(clusterStack, 'ZeebeStandaloneTestCluster', {
-      vpc: defaultVpc,
-      ecsCluster: ecsCluster,
-      securityGroups: [sg],
-      simpleMonitor: true,
-      hazelcastExporter: true,
-    });
-
-    // Prepare the stack for assertions.
-    let template = Template.fromStack(clusterStack);
-    template.resourceCountIs('AWS::EFS::FileSystem', 0);
-    template.resourceCountIs('AWS::EFS::MountTarget', 0);
-    template.resourceCountIs('AWS::ECS::Service', 1);
-    template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
-    template.resourceCountIs('AWS::ServiceDiscovery::PrivateDnsNamespace', 0);
-    template.hasResourceProperties('AWS::ECS::Cluster', {
-      ClusterName: 'test-cluster',
-    });
-    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
-      ContainerDefinitions: [
-        {
-          Name: 'zeebe-standalone',
-          Memory: 1024,
-          Image: 'ghcr.io/camunda-community-hub/zeebe-with-hazelcast-exporter:8.0.5',
-        },
-        {
-          Name: 'simple-monitor',
-          Memory: 1024,
-        },
-      ],
-    });
-
-  });
 
 
 });
